0bbb6d9df55f27ba208ad2a0b8bb698c12447d24
Legit-Ox <saahil.d@ahduni.edu.in>
2024-03-02 23:19:46 +0530

    Home page changes

--- involved/app/(routes)/(user)/user/7447/page.tsx
+++ involved/app/(routes)/(user)/user/7447/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import image1 from '@/public/backgroud_img.png';
 const UserProfile = () => {
     return (
--- involved/components/carousel.tsx
+++ involved/components/carousel.tsx
@@ -1,49 +1,41 @@
 // pages/index.tsx
-'use client'
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
+"use client";
+import React, { useState, useEffect } from "react";
+import Image from "next/legacy/image";
 
 import image1 from "@/public/images/images_1.jpeg";
 import image2 from "@/public/images/images_2.jpeg";
 import image3 from "@/public/images/images_3.jpeg";
 
 const Carousel: React.FC = () => {
- const images = [image1, image2, image3]; 
- const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [image1, image2, image3];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
- const nextImage = () => {
+  const nextImage = () => {
     setCurrentIndex((currentIndex + 1) % images.length);
- };
+  };
 
- const prevImage = () => {
+  const prevImage = () => {
     setCurrentIndex((currentIndex - 1 + images.length) % images.length);
- };
- useEffect(() => {
-  const timer = setInterval(() => {
-    nextImage();
-  }, 4000); 
+  };
+  useEffect(() => {
+    const timer = setInterval(() => {
+      nextImage();
+    }, 4000);
 
-  return () => clearInterval(timer);
-}, [currentIndex]); 
- return (
+    return () => clearInterval(timer);
+  }, [currentIndex]);
+  return (
     <div>
       <h1>Welcome to My Website</h1>
-      <Image 
+      <Image
         src={images[currentIndex]}
         alt={`Slide ${currentIndex + 1}`}
-        style = {{width: '100%', height: '70vh'}} // Adjust height as needed
+        style={{ width: "100%", height: "70vh" }} // Adjust height as needed
         objectFit="cover"
       />
-
-      
     </div>
-
-    
-
-
-
-
- );
+  );
 };
 
 export default Carousel;

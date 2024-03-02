b4997f190855776efed9a1ce264fb6218d52bc95
Legit-Ox <saahil.d@ahduni.edu.in>
2024-03-03 01:49:43 +0530

    Some changes

--- involved/app/(routes)/page.tsx
+++ involved/app/(routes)/page.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import Card from "@/components/card_home";
 import i1 from "@/public/images/images_1.jpeg";
 import i2 from "@/public/mountain.jpg";
+import { SparklesCore } from "@/components/ui/sparkles";
 
 import SilderImg03 from "@/public/mountain.jpg";
 import { StaticImageData } from "next/legacy/image";
 import ProgressSlider from "@/components/progress_slider";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
 interface CardProps {
   image: StaticImageData;
   title: string;
@@ -80,20 +82,77 @@ const HomePage: React.FC = () => {
       buttonIcon: SilderImg03,
     },
   ];
+  const projects = [
+    {
+      title: "Stripe",
+      description:
+        "A technology company that builds economic infrastructure for the internet.",
+      link: "https://stripe.com",
+    },
+    {
+      title: "Netflix",
+      description:
+        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+      link: "https://netflix.com",
+    },
+    {
+      title: "Google",
+      description:
+        "A multinational technology company that specializes in Internet-related services and products.",
+      link: "https://google.com",
+    },
+    {
+      title: "Meta",
+      description:
+        "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+      link: "https://meta.com",
+    },
+    {
+      title: "Amazon",
+      description:
+        "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+      link: "https://amazon.com",
+    },
+    {
+      title: "Microsoft",
+      description:
+        "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+      link: "https://microsoft.com",
+    },
+  ];
   return (
     <div>
       {/* <Carousel /> */}
       <div className="w-full max-w-6xl mx-auto px-4 md:px-6 py-24 ">
         <div className="flex-col justify-center items-center">
+          <div className="h-[40rem] snap-center relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md mb-20">
+            <div className="w-full absolute inset-0 h-screen">
+              <SparklesCore
+                id="tsparticlesfullpage"
+                background="transparent"
+                minSize={0.6}
+                maxSize={1.4}
+                particleDensity={100}
+                className="w-full h-full"
+                particleColor="#FFFFFF"
+              />
+            </div>
+            <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
+              Be a part of the change
+            </h1>
+          </div>
+          <div className="max-w-5xl mx-auto px-8">
+            <HoverEffect items={projects} />
+          </div>
           <p className="text-center text-black rounded-tr-xl font-bold font-mono text-6xl pb-10">
             Latest News
           </p>
           <ProgressSlider items={items} />
         </div>
       </div>
-      <div className="container-wrapper">
+      {/* <div className="container-wrapper">
         <CardContainer cardsData={cardData} />
-      </div>
+      </div> */}
     </div>
   );
 };

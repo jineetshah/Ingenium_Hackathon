6669a174f42641fe3e2787a81b3e83288a622995
jineetshah <jineet.s@ahduni.edu.in>
2024-03-02 20:49:55 +0530

    Navbar

---
+++ involved/app/(routes)/ngos/register/page.tsx
@@ -0,0 +1,14 @@
+// pages/ngo-registration.tsx
+import React from 'react';
+import NGORegistrationForm from '@/components/NGORegistrationForm';
+
+const NGORegistrationPage: React.FC = () => {
+  return (
+    <div>
+      <h1>NGO Registration</h1>
+      <NGORegistrationForm />
+    </div>
+  );
+};
+
+export default NGORegistrationPage;

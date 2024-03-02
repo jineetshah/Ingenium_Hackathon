43f29982d7908f0e6ea3275a90bf68711ce905f2
Deval <deval135darji@gmail.com>
2024-03-02 11:34:22 +0530

    configured shadcn

---
+++ involved/app/(routes)/(ngo)/dashboard/12345/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button"
+
+const NgoDashboard = () => {
+    return ( 
+        <div>
+            NGO dashboard
+            <Button variant="outline">Button</Button>
+
+        </div>
+    );
+}
+ 
+export default NgoDashboard;
--- involved/components/navbar.tsx
+++ involved/components/navbar.tsx
@@ -5,5 +5,5 @@ const Navbar = () => {
         </div>
     );
 }
- 
+
 export default Navbar;
